62e27c04e7c380860da85fad038921ef7259abcc
Bakston84 <Bakston1@yandex.ru>
2023-05-04 15:13:11 +0500

    HJ_04052023_1513

--- Task_001/Homework/src/main/java/org/example/homework/task_005/Main.java
+++ Task_001/Homework/src/main/java/org/example/homework/task_005/Main.java
@@ -1,4 +1,36 @@
 package org.example.homework.task_005;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите размер массива > ");
+            int num = scanner.nextInt();
+            int[] arr = new int[num];
+            int count = 0;
+            for (int index = 0; index < arr.length; index++) {
+                arr[index] = generateRandomInt(num);
+                if (arr[index] < 0)
+                count += index;
+                System.out.print(arr[index] + " ");
+            }
+            System.out.println(" ");
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < 0)
+                arr[i] = count;
+                System.out.print(arr[i] + " ");
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int generateRandomInt(int num) {
+        Random random = new Random();
+        return random.nextInt((num - (-num)) + 1) + (-num);
+    }
 }
