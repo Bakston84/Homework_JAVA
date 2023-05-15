d64ee4d44f9e36916fa6b8045f8f53917ff70b90
Bakston84 <Bakston1@yandex.ru>
2023-05-15 10:39:41 +0500

    HJ_15052023_1039

--- Task_001/Homework/src/main/java/org/example/homework/task_007/Main.java
+++ Task_001/Homework/src/main/java/org/example/homework/task_007/Main.java
@@ -1,5 +1,35 @@
 package org.example.homework.task_007;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите порядок символов > ");
+            String s = scanner.nextLine();
+            System.out.println("Порядок символов > " + isValid(s)); 
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            if ((s.charAt(i) == '(') || (s.charAt(i) == '[') || (s.charAt(i) == '{'))
+            st.push(s.charAt(i));
+            else
+            {
+                if (st.empty()) return false;
+                if ((s.charAt(i) == ')') && (st.peek() != '(')) return false;
+                if ((s.charAt(i) == ']') && (st.peek() != '[')) return false;
+                if ((s.charAt(i) == '}') && (st.peek() != '{')) return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }   
 }
