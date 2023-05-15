dc4297deac6b9226eb2e13828e20133255c28f79
Bakston84 <Bakston1@yandex.ru>
2023-05-15 09:31:01 +0500

    HJ_15052023_0930

--- Task_001/Homework/src/main/java/org/example/homework/task_006/Main.java
+++ Task_001/Homework/src/main/java/org/example/homework/task_006/Main.java
@@ -1,4 +1,20 @@
 package org.example.homework.task_006;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите размер LinkedList > ");
+            int n = scanner.nextInt();
+            for (int i = 0; i < n; i++) {
+                System.out.print("Введите число > ");
+                integerLinkedList.addFirst(scanner.nextInt());
+            }
+            scanner.close();             
+            System.out.println(integerLinkedList);
+        }
+    }
 }
---
+++ Task_001/Homework/src/main/java/org/example/homework/task_007/Main.java
@@ -0,0 +1,5 @@
+package org.example.homework.task_007;
+
+public class Main {
+    
+}
